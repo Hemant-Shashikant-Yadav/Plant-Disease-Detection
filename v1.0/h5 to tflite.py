759638e8ad10fba57ec307d272be7e913acbad67
Hemant Yadav <hemantsryadav>
2024-01-16 00:00:20 +0530

    final 15 jan

--- v1.0/h5 to tflite.py
+++ v1.0/h5 to tflite.py
@@ -6,6 +6,5 @@ model = tf.keras.models.load_model("tempMobileNet Transfer Learning2_1.h5")
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
-# Save the TFLite model to a file
 with open("../../../Program-backup/Pycharm ML DL/SIH/v1.1/model.tflite", "wb") as f:
     f.write(tflite_model)
