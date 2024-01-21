507a34451ae921a56848c0d6f390de45565f161f
hemant-sr-yadav <hemantsryadav@gmail.com>
2024-01-21 20:18:10 +0530

    21

--- temp.py
+++ temp.py
@@ -1,3 +1,8 @@
-import tensorflow as tf
+# import tensorflow as tf
+#
+# print(tf.config.list_physical_devices('GPU'))
+# print(tf.version.VERSION)
 
-print(tf.config.list_physical_devices('GPU'))
+import torch
+x = torch.rand(5, 3)
+print(x)
